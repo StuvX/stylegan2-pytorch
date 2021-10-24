142ca27b8e4d8a8ecba7a0c924dcd50e73eb951f
stuvx <wtnash@gmail.com>
2021-10-24 15:44:25 -0700

    remove label printout

--- train.py
+++ train.py
@@ -167,7 +167,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
             break
 
         real_img, label = next(loader)
-        print('label is {}'.format(label))
+        # print('label is {}'.format(label))
         if real_img is None: pass
         real_img = real_img.to(device)
 
